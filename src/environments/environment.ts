bbaa10f53c5f49e627152b0990ff941f9e8efc7a
OpenWork-NCE <christiannkengneemmanuel@gmail.com>
2025-04-17 21:04:03 +0100

    Fix route errors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://familyactivities-cubdbnh2g3g8cwfd.francecentral-01.azurewebsites.net',
+  apiUrl: 'https://familyactivities-cubdbnh2g3g8cwfd.francecentral-01.azurewebsites.net/api',
 };
