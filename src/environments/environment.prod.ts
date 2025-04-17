8b3640310f8b4352129482c51b97ce1adb56c970
OpenWork-NCE <christiannkengneemmanuel@gmail.com>
2025-04-17 20:34:22 +0100

    Add environnement variables options

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://familyactivities-cubdbnh2g3g8cwfd.francecentral-01.azurewebsites.net',
+  apiUrl: 'https://familyactivities-cubdbnh2g3g8cwfd.francecentral-01.azurewebsites.net/api',
 };
